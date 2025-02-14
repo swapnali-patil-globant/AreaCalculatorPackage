dc7efb0bf838bac09df97b113e1998bdc6db3586
swapnali.patil <swapnali.patil@globant.com>
2025-02-14 12:20:18 +0530

    Added area calculating methods

--- Sources/AreaCalculatorPackage/ShapeAreaCalculator.swift
+++ Sources/AreaCalculatorPackage/ShapeAreaCalculator.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+ 
+public class ShapeAreaCalculator {
+    static public func calculateCircleArea(radius: Double) -> Double {
+        return 2 * 3.14 * radius
+    }
+
+    static public func calculateSquareArea(side: Double) -> Double {
+        return side * side
+    }
+
+    static public func calculateRectangleArea(length: Double, breadth: Double) -> Double {
+        return length * breadth
+    }
+}
